ee9a5f20b5876c8f4755127435ab9b901c7f9695
lebed2045 <lebed2045@protonmail.com>
2019-04-13 16:39:58 -0700

    add createCheque

--- src/eth-wallet/EthereumHDWallet.test.ts
+++ src/eth-wallet/EthereumHDWallet.test.ts
@@ -1,6 +1,6 @@
 import EthereumHDWallet from "./EthereumHDWallet";
 
-describe("EthereumHDWallet", () => {
+describe("EthereumHDWallet", async () => {
 
     // BIP32, BIP39 and BIP44 are tested against data from https://www.coinomi.com/recovery-phrase-tool.html and testVector below
     const testnetMnemonic = "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong";
@@ -10,6 +10,15 @@ describe("EthereumHDWallet", () => {
         expect(ethereumHDWallet.accounts.length).toBeGreaterThan(10);
     });
 
+    // it("acceptCheque", async () => {
+    //     const response = await ethereumHDWallet.acceptCheque("da79b40d985b2dc4ca5b42a0bbd826552a48db822b365fd501e6f947e7dabc18");
+    //     console.log(ethereumHDWallet.getTxFee());
+    // });
+
+    it("HD", () => {
+       expect(ethereumHDWallet.accounts[0].privateKey.startsWith("7af65ba4dd53")).toBeTruthy();
+    });
+
     it("getUsdPrice", async () => {
         let result = await ethereumHDWallet.getUsdPrice();
         expect(result).toBeGreaterThan(100);
@@ -17,7 +26,27 @@ describe("EthereumHDWallet", () => {
 
     it("getBalance", async () => {
         const balance = await ethereumHDWallet.getBalance("0x04EBB7c0C5179041f83844CE79118Dd11B1da7FB");
-       expect(balance).toBeGreaterThan(0);
+        expect(balance).toBeGreaterThan(0);
     });
+
+    it("wasAddressUsed", async () => {
+        expect(await ethereumHDWallet.wasAddressUsed("0xde0b295668a9fd93d5f28d9ec85e40f4cb697bae")).toBe(false);
+        expect(await ethereumHDWallet.wasAddressUsed("0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae")).toBe(true);
+    });
+
+    it("accounts", ()=> {
+       const privateKey =  "0x95b6bd1166f910c971efce5df57fbbc9df6a7aed70070d733a442a7fb850c7e8";
+       const account = ethereumHDWallet.web3.eth.accounts.privateKeyToAccount(privateKey);
+       expect(account.address.toLocaleLowerCase()).toBe("0x543289b0965eba079b277b344dd1c0c2ab47a4ba");
+       expect(account.privateKey).toBe(privateKey);
+    });
+
+    // it("createCheque", async () => {
+    //    const cheque = await ethereumHDWallet.createCheque(0.0001);
+    //    console.log(cheque);
+    // });
+
+
+
 
 });
--- src/eth-wallet/EthereumHDWallet.ts
+++ src/eth-wallet/EthereumHDWallet.ts
@@ -2,6 +2,7 @@ import Bip39 from "bip39";
 // import {hdkey} from "ethereumjs-wallet/hdkey";
 import Axios from "axios";
 import Web3 from "web3";
+import EthTx from "ethereumjs-tx";
 
 export default class EthereumHDWallet {
     // BIP44 https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
@@ -64,7 +65,8 @@ export default class EthereumHDWallet {
         // http://api-ropsten.etherscan.io/api?module=account&action=txlist&address=0xde0b295668a9fd93d5f28d9ec85e40f4cb697bae
         const response = await Axios.get("http://api-ropsten.etherscan.io/api?module=account&action=txlist&address="+address);
         const responseJson = response.data;
-        return responseJson["status"] != "0";
+        // console.log(address, responseJson["status"]);
+        return responseJson["status"] !== "0";
     }
     async getBalance(address?: string): Promise<number> {
         address = address || this.accounts[0].address;
@@ -72,20 +74,107 @@ export default class EthereumHDWallet {
         return Number(balance);
     }
 
-    async createCheque(amount: number, password: string): Promise<string> {
-        // get new unused temp account
-        // let accountIndex = 1;
-        // while (this.getBalance(this.accounts[accountIndex].address))
-        // // create tx account -> temp account
-        // // send sendTransaction
-        // // encrypt private-key
-        // // return serialized
-
-        return "";
+    async signTransactionToIndex(index: number, amountWei: number) {
+        const account_1 = this.web3.eth.accounts.privateKeyToAccount("0x" + this.accounts[0].privateKey);
+        // console.log(index, this.accounts[index]);
+        const account_2 = this.web3.eth.accounts.privateKeyToAccount("0x" + this.accounts[index].privateKey);
+        // console.log(account_2.address);
+        const toAddress = account_2.address;
+        const fromPrivateKey = this.accounts[0].privateKey;
+        return await this.signTransaction(fromPrivateKey, toAddress, amountWei);
     }
 
-    async acceptCheque(serializedCheque: string, password: string): Promise<string> {
-        return "";
+    getGasPrise() {
+        return Number(this.web3.utils.toWei("2", "gwei"));
+    }
+
+    getGasLimit() {
+        return 21000;
+    }
+
+    getTxFee() {
+        return this.getGasPrise() * this.getGasLimit();
+    }
+
+    async signTransaction(fromPrivateKey: string, toAddress: string, amountWei: number) {
+        console.log("sign tx to", toAddress, amountWei);
+        // console.log("aa");
+        const txObject = {
+            nonce: this.web3.utils.toHex(await this.web3.eth.getTransactionCount(this.getAddress())),
+            to: toAddress,
+            value: this.web3.utils.toHex(amountWei),
+            gasLimit: this.web3.utils.toHex(this.getGasLimit()),
+            gasPrice: this.web3.utils.toHex(this.getGasPrise())
+        };
+        // Sign transaction
+        // console.log("a");
+        const tx = new EthTx(txObject);
+        // console.log("b");
+        const bufferPrivateKey1 = Buffer.from(fromPrivateKey, "hex");
+        // console.log("c");
+        tx.sign(bufferPrivateKey1);
+        // Serialize tx for sending it to the network
+        const serializedTx = tx.serialize();
+        // console.log("d");
+        const rawTx = "0x" + serializedTx.toString("hex");
+        // console.log("e");
+
+        return await this.sendRawTransaction(rawTx);
+    }
+
+    async sendRawTransaction(rawtx: string) {
+        console.log("sending rawTx", rawtx);
+        return new Promise((resolve, reject) => {
+            this.web3.eth.sendSignedTransaction(rawtx)
+                .on("transactionHash", (txHash) => {
+                    resolve(txHash);
+                })
+                .on("error", (e) => {
+                    reject(e);
+                })
+                .on("receipt", (m) => {
+                    console.log("receipt = ", m);
+                });
+        });
+    }
+
+    etherToWei(amountEther: number) : number {
+        return Number(this.web3.utils.toWei("" + amountEther, "ether"));
+    }
+
+    async getNextUnusedAccountIndex() {
+        let accountIndex = this.nextUnusedAddressIndex;
+        while (await this.wasAddressUsed(this.accounts[accountIndex].address) ) {
+            accountIndex += 1;
+            // console.log(accountIndex, this.accounts[accountIndex]);
+        }
+        this.nextUnusedAddressIndex = accountIndex;
+        console.log("nextUnusedAddressIndex", accountIndex);
+        return accountIndex;
+    }
+    async createCheque(amountEther: number, password = ""): Promise<string> {
+        const amountOfWei = this.etherToWei(amountEther);
+        // get new unused temp account
+        let accountIndex = await this.getNextUnusedAccountIndex();
+        // create tx account -> temp account
+        const response = this.signTransactionToIndex(accountIndex, amountOfWei);
+        // send sendTransaction
+        return encodeURI(this.accounts[accountIndex].privateKey);
+        // encrypt private-key
+        // return serialized
+    }
+
+    async acceptCheque(serializedCheque: string, password = ""): Promise<string> {
+        const stringifyObject = decodeURI(serializedCheque);
+        console.log(stringifyObject);
+        const privateKey = stringifyObject;
+        const chequeAccount = this.web3.eth.accounts.privateKeyToAccount("0x" + privateKey);
+        const balance = await this.getBalance(chequeAccount.address);
+        console.log(balance);
+        const chequeAmountWei = balance - this.getTxFee();
+        const response = await this.signTransaction(privateKey, this.getAddress(), chequeAmountWei);
+        console.log(response);
+        return "Accepted " + chequeAmountWei + " wei";
     }
 
     async getUsdPrice(): Promise<number> {
